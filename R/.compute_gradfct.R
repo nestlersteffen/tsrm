cccf2df810c9848f4713c79e2cb0d868aebd0911
Steffen Nestler <steffen.nestler@uni-muenster.de>
2026-05-19 18:11:29 +0200

    unified compute_gradient function

---
+++ R/.compute_gradfct.R
@@ -0,0 +1,103 @@
+
+#---- this function computes the log-likelihood across all groups: 
+
+compute_gradfct <- function( parm=NULL, parm_list=NULL, parm_table=NULL,
+	data_list=NULL, args_list=NULL, model=c("srm","tsrm"), both=TRUE ) 
+{
+	#- get model-specific functions.
+	model <- match.arg( model )
+	if ( model == "srm" ) {
+		include_free_parms <- srm_include_free_parameters
+        get_sigmas         <- srm_get_sigmas
+        sigma_derivatives  <- srm_sigma_derivatives
+	} else {
+		include_free_parms <- tsrm_include_free_parameters_v2
+        get_sigmas         <- tsrm_get_sigmas
+        sigma_derivatives  <- tsrm_sigma_derivatives
+	}
+
+	#- insert parm
+	parm_list <- include_free_parms( parm=parm, parm_list=parm_list, 
+		parm_table=parm_table )
+
+	#- get covariance matrices
+	parm_list <- get_sigmas( parm_list=parm_list )
+
+	#- get parm matrices:
+	BETA 	<- parm_list[["BETA"]]
+	SIGMA_G <- parm_list[["SIGMA_G"]]
+	SIGMA_P <- parm_list[["SIGMA_P"]]
+	SIGMA_D <- parm_list[["SIGMA_D"]]
+    SIGMA_T <- parm_list[["SIGMA_T"]]
+
+	#- get matrices and groupinfo:
+	groupinfo <- data_list[["groupinfo"]]
+	y  <- data_list[["y"]]
+	X  <- data_list[["X"]]
+	Zp <- data_list[["Zp"]]
+	Zd <- data_list[["Zd"]]
+	Zg <- data_list[["Zg"]] 
+	Zt <- data_list[["Zt"]] 
+	np <- data_list[["np"]]
+	nd <- data_list[["nd"]]
+	nt <- data_list[["nt"]]
+	nv <- data_list[["nv"]]
+	
+	#- make the matrices "big"
+	SIGMA_P <- diag(1,np) %x% SIGMA_P
+	SIGMA_D <- diag(1,nd) %x% SIGMA_D
+	SIGMA_T <- if ( ncol(Zt) > 0 ) diag(1, nt) %x% parm_list[["SIGMA_T"]]
+
+	#- get no. groups:
+	ngroups  <- nrow( groupinfo )
+	gradient <- matrix( 0, nrow = ngroups, ncol = max( parm_table$index) ) 
+	llfct    <- rep( 0, ngroups )
+
+	#- let's go:
+	for ( ng in seq( ngroups ) ) {
+			
+		#- get indices:
+		idx1 <- groupinfo[ng,5] - groupinfo[ng,4] + 1 
+		idx2 <- groupinfo[ng,5]
+
+		#- fixed groups?
+		BETA_ng <- BETA
+		parm_table_ng <- parm_table
+		if ( args_list$fixed_group ) {
+			if ( length( BETA ) > ngroups ) {
+				idx <- c( ng, (ngroups+1):length(BETA) )
+			} else {
+				idx <- ng 
+			}
+			BETA_ng <- as.matrix( BETA[idx,1] )
+			if ( length( which( parm_table_ng$type == "BETA" ) ) > 0 ) {
+				parm_table_ng <- parm_table[ ( parm_table$type == "BETA" & parm_table$pos1 %in% idx ) | parm_table$type != "BETA", ]
+				parm_table_ng[ parm_table_ng$type == "BETA", ]$pos1 <- seq( 1, nrow( BETA_ng ), 1 )
+			}	
+		}
+			
+		#- compute ...
+		Zg_ng <- if (ncol(Zg) > 0) Zg[idx1:idx2, ] else matrix(0, nrow = idx2 - idx1 + 1, ncol = 0)
+		Zt_ng <- if (ncol(Zt) > 0) Zt[idx1:idx2, ] else matrix(0, nrow = idx2 - idx1 + 1, ncol = 0)
+
+		tmp_grad <- compute_gradient_singlegroup( 
+			parm_list=parm_list, parm_table=parm_table_ng, 
+			np=np, nd=nd, nt=nt, 
+			y=y[idx1:idx2], X=X[idx1:idx2,], 
+			Zg=Zg_ng, Zp=Zp[idx1:idx2, ], Zd=Zd[idx1:idx2, ], Zt=Zt_ng,
+            BETA=BETA_ng, SIGMA_G=SIGMA_G, SIGMA_P=SIGMA_P, SIGMA_D=SIGMA_D, SIGMA_T=SIGMA_T, 
+			args_list=args_list, sigma_derivatives=sigma_derivatives )
+
+		llfct[ng]     <- tmp_grad[ 1]
+		gradient[ng,] <- tmp_grad[-1]
+
+	}
+	
+	#- output:
+	if ( both ) {
+		return( list( "objective"=-1*sum(llfct), "gradient"=-1*colSums(gradient) ) )
+	} else {
+		return( -1*colSums(gradient) )
+	}
+
+}
---
+++ R/.compute_gradfct_single_group.R
@@ -0,0 +1,90 @@
+
+#---- this function computes the log-likelihood and the gradient for a single group:
+
+compute_gradient_singlegroup <- function( parm_list=NULL, parm_table=NULL, 
+	np=NULL, nd=NULL, nt=NULL, 
+	y=NULL, X=NULL, Zg=NULL, Zp=NULL, Zd=NULL, Zt=NULL, 
+	BETA=NULL, SIGMA_G=NULL, SIGMA_P=NULL, SIGMA_D=NULL, SIGMA_T=NULL, 
+	args_list=FALSE, sigma_derivatives=NULL )
+{
+	
+	#- compute V:
+    V <- Zp %*% SIGMA_P %*% t(Zp) +
+         Zd %*% SIGMA_D %*% t(Zd)
+
+    if ( !is.null(Zt) ) {
+        V <- V + Zt %*% SIGMA_T %*% t(Zt)
+    }
+    if ( random_group && !is.null(Zg) ) {
+        V <- V + Zg %*% SIGMA_G %*% t(Zg)
+    }
+
+	iV <- base::solve( V )
+	P  <- iV
+	
+	#- residuals + ll:
+	ey    <- as.vector( y - X%*%BETA ) 
+	ei    <- crossprod( iV, ey )
+	ng_ll <- mvtnorm::dmvnorm( ey, rep(0,ncol(V)), as.matrix(V), log = TRUE )
+
+	#- REML:
+	if ( with_reml ) {
+		tmp   <- crossprod(X,iV)%*%X
+		ng_ll <- ng_ll - 0.5*determinant( tmp, logarithm=TRUE )$modulus[1]
+		P     <- iV - (iV%*%X)%*%base::solve(tmp)%*%(crossprod(X,iV))
+	}
+	
+	#- derivative for beta:
+	dBETA <- crossprod( ei, X )
+	
+	#- now compute the gradient for the group:
+  	NP      <- max( parm_table$index)
+  	NOP     <- nrow( parm_table ) 
+  	ng_grad <- rep( 0, NP )
+
+  	for ( nn in 1:NOP ) {
+  
+	  	#- get matrix type:
+	    free_nn   <- parm_table[nn,]
+	    type      <- free_nn$type
+	    pos       <- c( free_nn$pos1, free_nn$pos2 )
+	    index_nn  <- free_nn$index
+
+	    if ( type == "BETA" ) {
+
+			res <- dBETA[ pos[1] ] 		
+ 		
+ 		} else {
+ 	
+	 		#- get derivative:
+	 		sigma_derive <- sigma_derivatives( parm_list=parm_list,
+	      		type=type, pos=pos )
+	  
+		    #- compute derivative of V depending on matrix:
+		    if ( type %in% c("SD_P","RHO_P") ) { # c("SIGMA_P")
+		    	tmp <- diag(1, np ) %x% sigma_derive
+		    	Z   <- Zp
+		    } else if ( type %in% c("SD_D","RHO_D") ) { # c("SIGMA_D")
+		    	tmp <- diag(1, nd ) %x% sigma_derive
+		    	Z   <- Zd
+		    } else if ( type %in% c("SD_T","RHO_T") ) {  
+		    	tmp <- diag(1, nt ) %x% sigma_derive
+		    	Z   <- Zt
+		    } else if ( type %in% c("SD_G", "RHO_G") ) {
+                tmp <- sigma_derive                   
+                Z   <- Zg
+            }
+
+		    V_DERIVE <- Z%*%tmp%*%t(Z)
+		    pt1      <- sum( P * V_DERIVE )
+		    pt2 	 <- ( t( ei ) %*% V_DERIVE ) %*% ei 
+		    res 	 <- -0.5*( pt1 - pt2 )
+
+		}
+
+    	ng_grad[ index_nn ] <- ng_grad[ index_nn ] + res
+  
+  	}
+	
+	return( c( ng_ll, ng_grad ) )
+}
