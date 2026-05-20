ddae23650448a91f9f3eac070d5cfff06e944766
Steffen Nestler <steffen.nestler@uni-muenster.de>
2026-05-20 06:20:37 +0200

    First change data-preparation: bldiad and make_datalist

---
+++ R/.make_datalist.R
@@ -0,0 +1,26 @@
+
+#- this function creates a list that contains the design matrices and a matrix with group infos 
+
+make_datalist <- function( our_data=NULL, names_list=NULL, args_list=NULL, model=c("srm","tsrm") ) 
+{
+
+	#- Step 1: get model
+	model <- match.arg( model )
+
+	#- Step 2: select make_datalist_groups function depending on model:
+    make_datalist_groups <- switch( model,
+        "srm"  = srm_make_datalist_groups,
+        "tsrm" = tsrm_make_datalist_groups,
+        stop( paste( "The srm or the tsrm can be estimated." ) )
+    )
+
+	#- step 1: we first generate a list with group specific design matrices:
+  	data_list_groups <- make_datalist_groups( our_data=our_data, names_list=names_list, 
+  		args_list=args_list ) 
+
+  	#- step 2: now we combine the matrices - for version 1, ignore this step!
+  	data_list <- make_datalist_combine( data_list_groups=data_list_groups, 
+  		no_var=names_list$no_var )
+  	return( data_list )
+
+}
